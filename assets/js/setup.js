39c37d9a0d4eeee7068098688e16711366f1709c
LikeAaron <aaron.vanmarcke@student.howest.be>
2024-04-20 14:19:50 +0200

    fixed

--- assets/js/setup.js
+++ assets/js/setup.js
@@ -1,4 +1,9 @@
 //on domcontentload
-document.addEventListener('DOMContentLoaded', function() {
- // insert a video of the return of : 
+document.addEventListener('DOMContentLoaded', () => {
+const fill = document.querySelector('.fill');
+const loadButton = document.querySelector('.load-btn');
+    setTimeout(() => {
+    fill.style.width = '100%';
+    loadButton.removeAttribute('disabled');
+    }, 20000);
 });
