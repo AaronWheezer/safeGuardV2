bdefa568807ff6ed94f29067dcbdde94dcdd1b5c
LikeAaron <aaron.vanmarcke@student.howest.be>
2024-04-20 15:07:05 +0200

    fixed loading

--- assets/js/setup.js
+++ assets/js/setup.js
@@ -1,9 +1,16 @@
 //on domcontentload
-document.addEventListener('DOMContentLoaded', () => {
-const fill = document.querySelector('.fill');
-const loadButton = document.querySelector('.load-btn');
-    setTimeout(() => {
-    fill.style.width = '100%';
-    loadButton.removeAttribute('disabled');
-    }, 20000);
+//onload page
+window.addEventListener('load', () => {
+    fillBar();
 });
+
+function fillBar() {
+    const fill = document.querySelector('.fill');
+    const loadButton = document.querySelector('.load-btn');
+    fill.style.width = '100%';
+    loadButton.setAttribute('disabled', 'disabled');
+    setTimeout(() => {
+        //refere to home page
+        window.location.href = 'index.html';
+    }, 3000);
+  }
